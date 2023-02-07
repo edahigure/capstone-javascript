3c7c546d83da61cd80961de05d9c42b178c0d7d7
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-07 11:14:07 -0600

    Create category class to get the recipees from the API

---
+++ src/modules/Category.js
@@ -0,0 +1,20 @@
+export default class Category {
+  constructor(name) {
+    this.name = name;
+    this.meals = [];
+  }
+
+  getRecipees() {
+    return new Promise((resolve, reject) => {
+      fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${this.name}`)
+        .then((res) => res.json())
+        .then(({ meals }) => {
+          this.meals = meals;
+          resolve(meals);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+}
