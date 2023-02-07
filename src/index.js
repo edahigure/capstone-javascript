c46e1320257acec844a53f8ce614c88c6dcb3282
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-07 11:15:39 -0600

    Create categories instance, load category and add click event to each navbar button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './css/style.css';
+import Categories from './modules/Categories.js';
+
+window.onload = () => {
+  const meals = document.getElementById('meals');
+  const buttons = document.getElementById('listCategories');
+  const popupComments = document.getElementById('popupComments');
+  const categories = new Categories(meals, buttons, popupComments);
+
+  categories.load('Miscellaneous'); // category to load by default
+
+  const btnsLoadCategory = document.querySelectorAll('.btnsLoadCategory');
+  btnsLoadCategory.forEach((btnLoadCategory) => {
+    btnLoadCategory.addEventListener('click', () => {
+      const category = btnLoadCategory.textContent;
+      categories.load(category);
+    });
+  });
+};
