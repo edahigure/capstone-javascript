6798acd68697491fcfc6291a71474fbfd1524552
jorgeabrahan <jorge24abrahan@gmail.com>
2023-02-07 11:14:44 -0600

    Create meal class to return the html of each meal

---
+++ src/modules/Meal.js
@@ -0,0 +1,24 @@
+export default class Meal {
+  constructor(id, name, img) {
+    this.id = id;
+    this.name = name;
+    this.img = img;
+  }
+
+  html() {
+    const meal = document.createElement('div');
+    meal.id = this.id;
+    meal.innerHTML = `
+      <img src="${this.img}" alt="${this.name}" />
+      <div class="meal__headline">
+        <p>${this.name.length > 30 ? `${this.name.slice(0, 27)}...` : this.name}</p>
+        <button class="meal__like">
+          <span class="material-symbols-outlined"> favorite </span>
+          <span>2 likes</span>
+        </button>
+      </div>
+      <button class="button">Comments</button>
+    `;
+    return meal;
+  }
+}
