7eb33fd4d6a368a6e70f715b23a63f0e62a98d1c
Edahí Antonio Gutiérrez Reyes <edahigure@gmail.com>
2023-02-08 13:37:27 -0700

    Add some style

--- src/modules/Categories.js
+++ src/modules/Categories.js
@@ -3,14 +3,13 @@ import Meal from './Meal.js';
 
 export default class Categories {
   myId;
-
   constructor(container, buttons, popupComments) {
     this.container = container;
     this.buttons = buttons;
     this.popupComments = popupComments;
     this.categories = [];
     this.currentCategory = '';
-    this.myId = 'NJoITshHrkNNBBBobyAA';
+    this.myId = "NJoITshHrkNNBBBobyAA";
   }
 
   load(name) {
@@ -41,6 +40,7 @@ export default class Categories {
     });
   }
 
+
   getComments = async (id) => {
     const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${this.myId}/comments?item_id=${id}`;
     const result = await fetch(url, {
@@ -50,29 +50,33 @@ export default class Categories {
       },
     });
     return result;
+
   };
 
   initApp = async () => {
-    const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/';
+    const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/`;
     const result = await fetch(url, {
       method: 'POST',
       headers: {
         'Content-type': 'application/json; charset=UTF-8',
       },
     });
-    result.then((res) => { res.text().then((a) => console.log(a)); });
+
+    result.then((res) => { res.text().then((a) => console.log(a)) });
+
+
   };
 
-  addComment = async (itemId, name, comment) => {
+  addComment = async (item_id, name, comment) => {
     const url = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/${this.myId}/comments`;
     console.log(url);
     const result = fetch(url, {
       method: 'POST',
       body: JSON.stringify(
         {
-          item_id: itemId,
-          username: name,
-          comment,
+          "item_id": item_id,
+          "username": name,
+          "comment": comment
         },
       ),
       headers: {
@@ -80,109 +84,103 @@ export default class Categories {
       },
     });
 
-    const promise1 = result.then((res) => { res.text().then((a) => console.log(a)); });
+    const promise1 = result.then((res) => { res.text().then((a) => console.log(a)) });
     await promise1;
   }
 
   printComments = (comments) => {
     comments.then((res) => {
       res.json().then((data) => {
+        console.log('data length', data.length);
         const commentsTitle = this.popupComments.querySelector('.comments-title');
-        commentsTitle.innerHTML = `Commments (${data.length})`;
+        commentsTitle.innerHTML = 'Commments (' + data.length + ')';
         const commentsContainer = this.popupComments.querySelector('.comments-container');
-        commentsContainer.innerHTML = '';
+        commentsContainer.innerHTML='';
         let newData;
         for (let i = 0; i < data.length; i += 1) {
           newData = document.createElement('li');
-          newData.innerHTML = `${data[i].creation_date} ${data[i].username}: ${data[i].comment}`;
+          newData.innerHTML = `${data[i]['creation_date']} ${data[i]['username']}: ${data[i]['comment']}`;
           commentsContainer.appendChild(newData);
         }
-      });
+      })
     });
   }
+  
 
-  countComments = (comments) => {
-    comments.then((res) => {
-      res.json().then((data) => {
-        const commentsTitle = this.popupComments.querySelector('.comments-title');
-        commentsTitle.innerHTML = `Commments (${data.length})`;
-        const commentsContainer = this.popupComments.querySelector('.comments-container');
-        commentsContainer.innerHTML = '';
-        let newData;
-        for (let i = 0; i < data.length; i += 1) {
-          newData = document.createElement('li');
-          newData.innerHTML = `${data[i].creation_date} ${data[i].username}: ${data[i].comment}`;
-          commentsContainer.appendChild(newData);
-        }
-      });
-    });
-  }
 
   getItemApiMain = async (mealId) => {
-    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
-    const result = fetch(url);
 
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    const result =  fetch(url);
+    
     await result.then((response) => response.json()).then((json) => {
       console.log(json.meals[0]);
 
       const popupImg = this.popupComments.querySelector('#popupImage');
-      popupImg.src = json.meals[0].strMealThumb;
+      popupImg.src = json.meals[0]['strMealThumb'];
 
       const popupTitle = this.popupComments.querySelector('#popupTitle');
-      popupTitle.innerHTML = json.meals[0].strMeal;
+      popupTitle.innerHTML = json.meals[0]['strMeal'];
 
       const gridContainer = this.popupComments.querySelector('.grid-container');
-      gridContainer.innerHTML = '';
+      gridContainer.innerHTML='';
       let newData = document.createElement('div');
-      newData.innerHTML = `Area:${json.meals[0].strArea}`;
+      newData.innerHTML = 'Area:' + json.meals[0]['strArea'];
       gridContainer.appendChild(newData);
 
       newData = document.createElement('div');
-      newData.innerHTML = `Category:${json.meals[0].strCategory}`;
+      newData.innerHTML = 'Category:' + json.meals[0]['strCategory'];
       gridContainer.appendChild(newData);
 
+      
       const ingredientContainer = document.createElement('ul');
-      ingredientContainer.className = 'ingredient-container';
+      ingredientContainer.className='ingredient-container';
       gridContainer.appendChild(ingredientContainer);
-      ingredientContainer.innerHTML = '';
+      ingredientContainer.innerHTML='';
 
       newData = document.createElement('li');
       newData.innerHTML = 'Ingredients:';
       ingredientContainer.appendChild(newData);
 
-      const strIngredient = [];
+      let strIngredient = [];
       for (let i = 1; i <= 20; i += 1) {
-        strIngredient[i] = json.meals[0][`strIngredient${i.toString()}`];
+        strIngredient[i] = json.meals[0]['strIngredient' + i.toString()];
         if (strIngredient[i] === '') { break; }
         newData = document.createElement('li');
         newData.innerHTML = strIngredient[i];
         ingredientContainer.appendChild(newData);
       }
 
+
       const measureContainer = document.createElement('ul');
-      measureContainer.className = 'ingredient-container';
+      measureContainer.className='ingredient-container';
       gridContainer.appendChild(measureContainer);
-      measureContainer.innerHTML = '';
+      measureContainer.innerHTML='';
 
       newData = document.createElement('li');
       newData.innerHTML = 'Maesures:';
       measureContainer.appendChild(newData);
 
-      const strMeasure = [];
+      let strMeasure = [];
       for (let i = 1; i <= 20; i += 1) {
-        strMeasure[i] = json.meals[0][`strMeasure${i.toString()}`];
+        strMeasure[i] = json.meals[0]['strMeasure' + i.toString()];
         if (strMeasure[i] === '') { break; }
         newData = document.createElement('li');
         newData.innerHTML = strMeasure[i];
         measureContainer.appendChild(newData);
       }
 
-      const instructions = document.querySelector('.instructions');
-      instructions.innerHTML = json.meals[0].strInstructions;
+      const instructions=document.querySelector(".instructions");
+      instructions.innerHTML=json.meals[0]['strInstructions'];
+
 
       const comments = this.getComments(mealId);
       this.printComments(comments);
+
     });
+
+   
+
   };
 
   displayPopupComments(mealId) {
